8b3ce7525b28f7585538bad1f35ea3a54845df70
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2021-12-02 18:53:07 -0500

    Days 1 and 2

--- Day01.fs
+++ Day01.fs
@@ -1,7 +1,57 @@
 module Day01
 
-let part_1 () = printfn "Oh yeah"
+open System.IO
 
-let part_2 () = printfn "Oh yeah 2"
+let parse (x: string) = x.TrimEnd() |> uint
+
+let count_increase (data: uint []) =
+    data
+    |> Array.pairwise
+    |> Array.sumBy (fun (first, second) -> if first < second then 1 else 0)
+
+let triple_sum_increase (data: uint []) =
+    data
+    |> Array.windowed 3
+    |> Array.map (fun x -> Array.sum x)
+    |> Array.pairwise
+    |> Array.sumBy (fun (first, second) -> if first < second then 1 else 0)
+
+let input =
+    File.ReadLines("puzzle_input/day_01/part1")
+    |> Array.ofSeq
+    |> Array.map parse
+
+let part_1 () = input |> count_increase |> printfn "%A"
+
+let part_2 () =
+    input |> triple_sum_increase |> printfn "%A"
 
 let parts = (1, part_1, part_2)
+
+module Tests =
+    let testInput =
+        ("199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263")
+            .Split '\n'
+        |> Array.map parse
+
+    let ``Example data works with count_increase`` () =
+
+        let result = testInput |> count_increase
+
+        assert (result = 7)
+
+    let ``Example data works with triple_sum_increase`` () =
+        let result = triple_sum_increase testInput
+        assert (result = 5)
+
+Tests.``Example data works with count_increase`` ()
+Tests.``Example data works with triple_sum_increase`` ()
---
+++ Day02.fs
@@ -0,0 +1,78 @@
+module Day02
+
+open System.IO
+
+type Dir =
+    | Forward of int
+    | Up of int
+    | Down of int
+
+let parse (line: string) =
+    match line.Trim().Split ' ' with
+    | [| "forward"; x |] -> Forward(int x)
+    | [| "down"; x |] -> Down(int x)
+    | [| "up"; x |] -> Up(int x)
+    | x -> failwithf "%A is not a valid pattern" x
+
+let processPosition (dirs: Dir []) =
+    dirs
+    |> Array.fold
+        (fun (x, y) ->
+            fun dir ->
+                match dir with
+                | Forward (by) -> (x + by, y)
+                | Up (by) -> (x, y - by)
+                | Down (by) -> (x, y + by)
+
+            )
+        (0, 0)
+
+let processPositionWithAim (dirs: Dir []) =
+    dirs
+    |> Array.fold
+        (fun (hPos, depth, aim) ->
+            fun dir ->
+                match dir with
+                | Down (x) -> (hPos, depth, aim + x)
+                | Up (x) -> (hPos, depth, aim - x)
+                | Forward (x) -> (hPos + x, depth + (aim * x), aim))
+        (0, 0, 0)
+
+
+let input =
+    File.ReadAllLines "puzzle_input/day_02"
+    |> Array.ofSeq
+    |> Array.map parse
+
+let part_1 () =
+    let (x, y) = input |> processPosition
+    printfn "%A" (x * y)
+
+let part_2 () =
+    let (x, y, _) = input |> processPositionWithAim
+    printfn "%A" (x * y)
+
+let parts = (2, part_1, part_2)
+
+module Tests =
+    let testInput =
+        ("forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2")
+            .Split '\n'
+        |> Array.map parse
+
+    let ``part 1 sample data`` () =
+        let result = testInput |> processPosition
+        assert (result = (15, 10))
+
+    let ``part 2 sample data`` () =
+        let (depth, hPos, _) = testInput |> processPositionWithAim
+        printfn "%A" (depth, hPos)
+        assert ((depth, hPos) = (15, 60))
+
+Tests.``part 1 sample data`` ()
+Tests.``part 2 sample data`` ()
